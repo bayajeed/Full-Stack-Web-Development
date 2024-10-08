e5749b0a561bb4edb5d2a9b5028bb5a2e4777e19
bayajeed <bayajeedhossain@gmail.com>
2024-10-09 00:19:09 +0600

    First Commit

---
+++ Python/test.py
@@ -0,0 +1,22 @@
+# var_1 = int(input("X: "))
+# var_2 = int(input("Y: "))
+# if var_1 is var_2:
+#     print("Same Value")
+# elif var_2 > var_1:
+#     print("Y is Big")
+# else:
+#     print("X is Big")
+
+# var_1 = int(input("X: "))
+# result = var_1 % 2
+# if result == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# var = 5==9 or 5<6
+# var_int = int(var)
+# print(var)
+# # print(var[-2])
+# print(type(var))
+# print(type(var_int))
